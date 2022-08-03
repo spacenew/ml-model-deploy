7f3a099c7bcd66bee5bad5906e8d21166c3848d6
ilija <68110781+spacenew@users.noreply.github.com>
2022-08-03 16:15:26 +0300

    Add files via upload

--- streamlit_app_my.py
+++ streamlit_app_my.py
@@ -31,18 +31,18 @@ answers = {}
 expander = st.expander("👤 Personal Information")
 
 with expander:
-    answers_dict['age'] = st.slider('What is your age?', help = 'The slider can be moved using the arrow keys.', min_value = 0, max_value = 100, step = 1)
+    answers['age'] = st.slider('What is your age?', help = 'The slider can be moved using the arrow keys.', min_value = 0, max_value = 100, step = 1)
 
-    answers_dict['sex'] = 1 if st.selectbox('What is your gender?', ['Female', 'Male']) == 'Yes' else 0
+    answers['sex'] = 1 if st.selectbox('What is your gender?', ['Female', 'Male']) == 'Yes' else 0
 
-    answers_dict['bmi'] = st.slider('What is your BMI (Body Mass Index)?', help = 'The slider can be moved using the arrow keys.', min_value = 0.0, max_value = 100.0, step = 0.1)
+    answers['bmi'] = st.slider('What is your BMI (Body Mass Index)?', help = 'The slider can be moved using the arrow keys.', min_value = 0.0, max_value = 100.0, step = 0.1)
 
-    answers_dict['children'] = st.slider('How many children do you have?', help = 'The slider can be moved using the arrow keys.', min_value = 0, max_value = 10, step = 1)
+    answers['children'] = st.slider('How many children do you have?', help = 'The slider can be moved using the arrow keys.', min_value = 0, max_value = 10, step = 1)
 
-    answers_dict['smoker'] = 1 if st.selectbox('Do you smoke?', ['Yes', 'No']) == 'Yes' else 0
+    answers['smoker'] = 1 if st.selectbox('Do you smoke?', ['Yes', 'No']) == 'Yes' else 0
 
-    answers_dict['region'] = st.selectbox('In what US region do you live?', ['Southwest', 'Southeast', 'Northwest', 'Northeast'])
+    answers['region'] = st.selectbox('In what US region do you live?', ['Southwest', 'Southeast', 'Northwest', 'Northeast'])
 
 if st.button('Predict Value'):
-    value = predict(answers_dict)
+    value = predict(answers)
     st.write(f'Your insurance would cost {round(value, 2)} USD.')
